c9f12ffc719fdb6c7e028bbc88b65eaab2d0d2ef
eabowman <elizabeth.bowman@austin.utexas.edu>
2020-11-10 20:27:45 -0600

    Completed analyses and figure creation for question 3 how does length of autoclave time affect soil characteristics.

--- scripts/AutoclaveEffectAnalysis.R
+++ scripts/AutoclaveEffectAnalysis.R
@@ -8,40 +8,206 @@ library(devtools)
 library(tidyverse)
 library(ggbiplot)
 
+# change number of decimal points format to 4
+options(digits = 4)
+
 soil.data <- read.csv('data/data.autoclave.effect.csv', as.is = T)
 
 # log transform non-normal data
 soil.data %>%
-  mutate(log.EC = log(Cond..umhos.cm.),
-         log.P = log(P..ppm.),
-         log.S = log(S..ppm.)) -> soil.data
+  mutate(log.EC = log(EC),
+         log.P = log(P),
+         log.S = log(S)) -> soil.data
 
-#--Check for differences based on invasion
-# Isolate unautoclaved data
-soil.data[soil.data$autoclave.time == 0,] -> soil.invasion.data
+# --------------------------------------------------------------------------#
+#--Soil characteristics as a function of autoclave time: One Way ANOVA ----
+# --------------------------------------------------------------------------#
 
-# T-test
-t.ph <- t.test(pH ~ invasion, data = soil.invasion.data) # p = 0.1
-t.EC <- t.test(log.EC ~ invasion, data = soil.invasion.data) # p = 0.09
-t.no3 <- t.test(NO3N..ppm. ~ invasion, data = soil.invasion.data) 
-# p = 0.0002***** all data, just non-autoclaved = 0.16
+# Make results table
+autoclave.results <- data.frame(soil.characteristics = 
+                                  c('pH','log.EC','Nitrate','log.P','K','Mg',
+                                    'log.S','Na','Ca'),
+                                df.1 = NA,
+                                df.2 = NA,
+                                F.stat = NA,
+                                p = NA)
 
-t.p <- t.test(log.P ~ invasion, data = soil.invasion.data) # p = 0.51
-t.K <- t.test(K..ppm. ~ invasion, data = soil.invasion.data) 
-# p = 0.05****** all data, just non-autoclaved = 0.45
+# pH
+lm.pH <- lm(pH ~ autoclave.time, data = soil.data)
+anova.pH <- anova(lm.pH)
+autoclave.results[autoclave.results$soil.characteristics == 'pH',
+                    'df.1'] <- anova.pH$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'pH',
+                    'df.2'] <- anova.pH$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'pH',
+                    'F.stat'] <- anova.pH$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'pH',
+                    'p'] <- anova.pH$`Pr(>F)`[1]
 
-t.Ca <- t.test(Ca..ppm. ~ invasion, data = soil.invasion.data) # p = 0.09
-t.Mg <- t.test(Mg..ppm. ~ invasion, data = soil.invasion.data) 
-# p = 0.01****** all data, just non-autoclaved = 0.19
+# log.EC
+lm.EC <- lm(log.EC ~ autoclave.time, data = soil.data)
+anova.EC <- anova(lm.EC)
+autoclave.results[autoclave.results$soil.characteristics == 'log.EC',
+                    'df.1'] <- anova.EC$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'log.EC',
+                    'df.2'] <- anova.EC$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'log.EC',
+                    'F.stat'] <- anova.EC$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'log.EC',
+                    'p'] <- anova.EC$`Pr(>F)`[1]
 
-t.S <- t.test(log.S ~ invasion, data = soil.invasion.data) # p = 0.63
-t.Na <- t.test(Na..ppm. ~ invasion, data = soil.invasion.data) # p = 0.96
+# Nitrate
+lm.N <- lm(Nitrate ~ autoclave.time, data = soil.data)
+anova.N <- anova(lm.N)
+autoclave.results[autoclave.results$soil.characteristics == 'Nitrate',
+                    'df.1'] <- anova.N$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Nitrate',
+                    'df.2'] <- anova.N$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'Nitrate',
+                    'F.stat'] <- anova.N$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Nitrate',
+                    'p'] <- anova.N$`Pr(>F)`[1]
 
-# PCA
-soil.pca <- prcomp(soil.invasion.data[c('pH','log.EC','NO3N..ppm.','log.P',
-                               'K..ppm.','Ca..ppm.','Mg..ppm.',
-                               'log.S','Na..ppm.')],
-                   center = T, scale. = T)
-summary(soil.pca)
-ggbiplot(soil.pca, groups = soil.invasion.data$invasion) +
-  theme_bw()
+# log.P
+lm.P <- lm(log.P ~ autoclave.time, data = soil.data)
+anova.P <- anova(lm.P)
+autoclave.results[autoclave.results$soil.characteristics == 'log.P',
+                    'df.1'] <- anova.P$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'log.P',
+                    'df.2'] <- anova.P$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'log.P',
+                    'F.stat'] <- anova.P$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'log.P',
+                    'p'] <- anova.P$`Pr(>F)`[1]
+
+# K
+lm.K <- lm(K ~ autoclave.time, data = soil.data)
+anova.K <- anova(lm.K)
+autoclave.results[autoclave.results$soil.characteristics == 'K',
+                    'df.1'] <- anova.K$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'K',
+                    'df.2'] <- anova.K$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'K',
+                    'F.stat'] <- anova.K$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'K',
+                    'p'] <- anova.K$`Pr(>F)`[1]
+
+# Mg
+lm.Mg <- lm(Mg ~ autoclave.time, data = soil.data)
+anova.Mg <- anova(lm.Mg)
+autoclave.results[autoclave.results$soil.characteristics == 'Mg',
+                    'df.1'] <- anova.Mg$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Mg',
+                    'df.2'] <- anova.Mg$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'Mg',
+                    'F.stat'] <- anova.Mg$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Mg',
+                    'p'] <- anova.Mg$`Pr(>F)`[1]
+
+# log.S
+lm.S <- lm(log.S ~ autoclave.time, data = soil.data)
+anova.S <- anova(lm.S)
+autoclave.results[autoclave.results$soil.characteristics == 'log.S',
+                    'df.1'] <- anova.S$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'log.S',
+                    'df.2'] <- anova.S$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'log.S',
+                    'F.stat'] <- anova.S$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'log.S',
+                    'p'] <- anova.S$`Pr(>F)`[1]
+
+# Na
+lm.Na <- lm(Na ~ autoclave.time, data = soil.data)
+anova.Na <- anova(lm.Na)
+autoclave.results[autoclave.results$soil.characteristics == 'Na',
+                    'df.1'] <- anova.Na$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Na',
+                    'df.2'] <- anova.Na$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'Na',
+                    'F.stat'] <- anova.Na$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Na',
+                    'p'] <- anova.Na$`Pr(>F)`[1]
+
+# Ca
+lm.Ca <- lm(Ca ~ autoclave.time, data = soil.data)
+anova.Ca <- anova(lm.Ca)
+autoclave.results[autoclave.results$soil.characteristics == 'Ca',
+                    'df.1'] <- anova.Ca$Df[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Ca',
+                    'df.2'] <- anova.Ca$Df[2]
+autoclave.results[autoclave.results$soil.characteristics == 'Ca',
+                    'F.stat'] <- anova.Ca$`F value`[1]
+autoclave.results[autoclave.results$soil.characteristics == 'Ca',
+                    'p'] <- anova.Ca$`Pr(>F)`[1]
+
+write.csv(autoclave.results, 'results/Autoclave_results.csv', row.names = F)
+
+
+# --------------------------------------------------------------------------#
+#--Soil characteristics as a function of autoclave time: Graphs---
+#-- Log.EC, Log.P, Log.S, Na
+# --------------------------------------------------------------------------#
+soil.data$autoclave.time <- factor(soil.data$autoclave.time)
+
+##****Check outliers
+
+# Log EC
+graph.logEC <- ggplot(soil.data, aes(x = autoclave.time,
+                                     y = log.EC)) +
+  geom_boxplot() +
+  ylab('Log EC (umhos/cm)') +
+  xlab('Autoclave length (min)') +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) +
+  theme(axis.text = element_text(color = "black"))
+  
+ggsave('figures/logEC_AutoclaveTime.tiff', device = 'tiff', plot = graph.logEC,
+       width = 6, height = 6, units = 'cm', dpi = 300)
+
+# Log P
+graph.logP <- ggplot(soil.data, aes(x = autoclave.time,
+                                     y = log.P)) +
+  geom_boxplot() +
+  ylab('Log phosphorus (ppm)') +
+  xlab('Autoclave length (min)') +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) +
+  theme(axis.text = element_text(color = "black"))
+  
+ggsave('figures/logP_AutoclaveTime.tiff', device = 'tiff', plot = graph.logP,
+       width = 6, height = 6, units = 'cm', dpi = 300)
+
+# Log S
+graph.logS <- ggplot(soil.data, aes(x = autoclave.time,
+                                     y = log.S)) +
+  geom_boxplot() +
+  ylab('Log sulfur (ppm)') +
+  xlab('Autoclave length (min)') +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) +
+  theme(axis.text = element_text(color = "black"))
+  
+ggsave('figures/logS_AutoclaveTime.tiff', device = 'tiff', plot = graph.logS,
+       width = 6, height = 6, units = 'cm', dpi = 300)
+
+# Na
+graph.Na <- ggplot(soil.data, aes(x = autoclave.time,
+                                     y = Na)) +
+  geom_boxplot() +
+  ylab('Sodium (ppm)') +
+  xlab('Autoclave length (min)') +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black")) +
+  theme(axis.text = element_text(color = "black"))
+  
+ggsave('figures/Na_AutoclaveTime.tiff', device = 'tiff', plot = graph.Na,
+       width = 6, height = 6, units = 'cm', dpi = 300)
+
